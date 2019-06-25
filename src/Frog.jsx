776eca6053fee0515fffa32e95180afdb41a41d2
peter <piotr.ludwig@typethink.com>
2019-06-25 17:39:41 +0200

    Frog jump functionality

--- src/Frog.jsx
+++ src/Frog.jsx
@@ -5,44 +5,34 @@ import "./styles.css";
 class Frog extends Component {
   jump = newPosition => {
     let { fields } = this.props;
-    let frog = { ...fields[this.props.lane][this.props.position] };
-    // fields[this.props.lane][this.props.position].type = "field";
+    let field = { ...fields[this.props.lane][this.props.position] };
+    let frog =  field.onField
     let jumpTo = this.getJumpPosition();
-    console.log("TCL: Frog -> jumpTo xx77", jumpTo)
 
     if (jumpTo !== undefined) {
+      fields[this.props.lane][this.props.position].empty = true
+      fields[this.props.lane][this.props.position].onField = {}
       frog.lane = jumpTo.lane;
       frog.position = jumpTo.position;
-      fields[jumpTo.lane][jumpTo.position] = frog;
+      fields[jumpTo.lane][jumpTo.position].empty = false
+      fields[jumpTo.lane][jumpTo.position].onField = frog;
       this.props.updateLake(fields);
       this.props.updateLake(this.props.cleanCheck(fields));
     } else {
-      // this.props.updateLake(this.props.cleanCheck(fields));
+      this.props.updateLake(this.props.cleanCheck(fields));
+      alert('You can not jump on another frog')
     }
   };
 
   getJumpPosition = () => {
     let { fields } = this.props;
     let jumpTo = {};
-    // fields.map(lakeLane => {
-    //   lakeLane.map(field => {
-    //     // console.log("TCL: Frog -> getJumpPosition -> field", fiel
-    //     if (field.check === true && field.empty === true && field.type === 'field') {
-    //       console.log('field TRUE', field.lane)
-    //       jumpTo.lane = field.lane;
-    //       jumpTo.position = field.position;
-    //     }
-    //   });
-    // });
     for (let i = 0; i < fields.length; i++){
       for (let a = 0; a < fields[i].length; a++){
         if (fields[i][a].check === true && fields[i][a].empty === true && fields[i][a].type === 'field') {
-          // console.log('field TRUE', fields.lane)
           jumpTo.lane = fields[i][a].lane;
           jumpTo.position = fields[i][a].position;
           return jumpTo;
-
-          break;
         }
       }
     }
